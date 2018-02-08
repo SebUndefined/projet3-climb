44d7554a5e59e48d3e7af82dc85eb95d63a636c0
SebUndefined <me@sebundefined.fr>
2018-02-08 20:38:37 +0100

    Start here !

---
+++ climb-consumer/src/main/java/fr/sebundefined/projet3/consumer/App.java
@@ -0,0 +1,13 @@
+package fr.sebundefined.projet3.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
